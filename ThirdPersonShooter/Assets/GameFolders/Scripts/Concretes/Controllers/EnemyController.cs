d72289e0b7321226ba53296a44ff4285f8ad42f1
emiruyan <emir-uyn@hotmail.com>
2022-05-25 22:29:15 +0300

    Enemy Model
    Enemy Model oluşturduk. NavMeshAgent ve CapsuleCollider componentlerini Enemymize atadık.

---
+++ ThirdPersonShooter/Assets/GameFolders/Scripts/Concretes/Controllers/EnemyController.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ThirdPersonShooter.Controllers
+{
+    public class EnemyController : MonoBehaviour
+    {
+        
+    }
+}
+
+
